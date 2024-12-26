78e36a98d22f4829a2a4e9ec8cb59596daaaabef
KlimovaED <klimovaed@icloud.com>
2024-12-26 15:05:26 +0100

    ..

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import '../styles/globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
