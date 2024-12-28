c00370451a0458632f279073a251214cc4554743
KlimovaED <klimovaed@icloud.com>
2024-12-28 16:31:17 +0100

    checkbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
 // "lint": "next lint",
-export default function Home() {
-  return <div>Hello</div>
+
+import { Button } from '@/src/components/button/Button'
+import { CheckBox } from '@/src/components/checkbox/CheckBox'
+
+const Home = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <Button>Send</Button>
+      <hr />
+      <CheckBox checked />
+    </div>
+  )
 }
+export default Home
