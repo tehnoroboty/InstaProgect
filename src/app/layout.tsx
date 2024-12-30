8da79fee1441e7de04f4f368bc6021a0d353d941
KlimovaED <klimovaed@icloud.com>
2024-12-30 15:52:38 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { Metadata } from 'next'
 import '../styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource-variable/inter'
+
+export const metadata: Metadata = {
+  title: 'Momenttify',
+  icons: {
+    icon: [
+      { url: '/logo/light.svg', media: '(prefers-color-scheme: dark)' },
+      { url: '/logo/dark.svg', media: '(prefers-color-scheme: light)' },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
