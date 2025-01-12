3c22631494f22d789866ed4d8407fa1bfdd29880
Andrei Huryn <gurin.nord@gmail.com>
2025-01-12 11:36:43 +0300

    refactor: format code

--- src/components/select/SelectBox.tsx
+++ src/components/select/SelectBox.tsx
@@ -1,60 +1,59 @@
-import {ComponentPropsWithoutRef} from 'react'
+import { ComponentPropsWithoutRef } from 'react'
 
+import Arrow from '@/src/assets/componentsIcons/ArrowIosDownOutline'
 import * as Select from '@radix-ui/react-select'
 
 import styles from './Select.module.scss'
 
-import {SelectItem} from './SelectItem'
-import Arrow from '@/src/assets/componentsIcons/ArrowIosDownOutline'
+import { SelectItem } from './SelectItem'
 
 export type Options = {
-    value: string
-    valueTitle: string
+  value: string
+  valueTitle: string
 }
 
 type Props = {
-    label?: string
-    placeholder: string
-    options: Options[]
-    size?: 'small' | 'medium' | 'large'
+  label?: string
+  options: Options[]
+  placeholder: string
+  size?: 'large' | 'medium' | 'small'
 } & ComponentPropsWithoutRef<'button'>
 
-export const SelectBox = ({label, placeholder = 'Select', options, size, ...rest}: Props) => {
+export const SelectBox = ({ label, options, placeholder = 'Select', size, ...rest }: Props) => {
+  return (
+    <Select.Root>
+      <Select.Group>
+        {label && <Select.Label className={styles.label}>{label}</Select.Label>}
+        <Select.Trigger
+          className={`${styles.trigger} ${size ? styles[size] : ''} ${label && styles.triggerLabel}`}
+          {...rest}
+          aria-label={placeholder}
+        >
+          <Select.Value placeholder={placeholder} />
+          <Select.Icon asChild>
+            <Arrow className={styles.icon} />
+          </Select.Icon>
+        </Select.Trigger>
+      </Select.Group>
 
-    return (
-        <Select.Root>
+      <Select.Portal>
+        <Select.Content
+          avoidCollisions
+          className={styles.content}
+          position={'popper'}
+          sideOffset={-1}
+        >
+          <Select.Viewport>
             <Select.Group>
-                {label && <Select.Label className={styles.label}>{label}</Select.Label>}
-                <Select.Trigger
-                    className={`${styles.trigger} ${size ? styles[size] : ''} ${label && styles.triggerLabel}`}
-                    {...rest}
-                    aria-label={placeholder}
-                >
-                    <Select.Value placeholder={placeholder}/>
-                    <Select.Icon asChild>
-                        <Arrow className={styles.icon}/>
-                    </Select.Icon>
-                </Select.Trigger>
+              {options.map(item => (
+                <SelectItem key={item.value} value={item.value}>
+                  {item.valueTitle}
+                </SelectItem>
+              ))}
             </Select.Group>
-
-            <Select.Portal>
-                <Select.Content
-                    avoidCollisions
-                    className={styles.content}
-                    position={'popper'}
-                    sideOffset={-1}
-                >
-                    <Select.Viewport>
-                        <Select.Group>
-                            {options.map(item => (
-                                <SelectItem key={item.value} value={item.value}>
-                                    {item.valueTitle}
-                                </SelectItem>
-                            ))}
-                        </Select.Group>
-                    </Select.Viewport>
-                </Select.Content>
-            </Select.Portal>
-        </Select.Root>
-    )
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  )
 }
