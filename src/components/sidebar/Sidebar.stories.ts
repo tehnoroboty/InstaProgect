637bdbb7906561168ec57442498235bc7a7acb45
KlimovaED <klimovaed@icloud.com>
2025-01-13 19:45:08 +0100

    update

--- src/components/sidebar/Sidebar.stories.ts
+++ src/components/sidebar/Sidebar.stories.ts
@@ -4,25 +4,25 @@ import { Home, HomeLine, PlusSquare, PlusSquareOutline } from '@/src/assets/comp
 import Sidebar, { MenuItemType } from '@/src/components/sidebar/Sidebar'
 
 const meta = {
-    component: Sidebar,
-    tags: ['autodocs'],
-    title: 'Components/Sidebar',
+  component: Sidebar,
+  tags: ['autodocs'],
+  title: 'Components/Sidebar',
 } satisfies Meta<typeof Sidebar>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 const menuItemsMock: MenuItemType[] = [
-    { href: '/', icon: HomeLine, iconActive: Home, title: 'Home' },
-    { href: '/create', icon: PlusSquareOutline, iconActive: PlusSquare, title: 'Create' },
+  { href: '/', icon: HomeLine, iconActive: Home, title: 'Home' },
+  { href: '/create', icon: PlusSquareOutline, iconActive: PlusSquare, title: 'Create' },
 ]
 
 export const Primary: Story = {
-    args: {
-        menuItems: {
-            additional: [],
-            mainActions: menuItemsMock,
-            usersActions: [],
-        },
+  args: {
+    menuItems: {
+      additional: [],
+      mainActions: menuItemsMock,
+      usersActions: [],
     },
+  },
 }
