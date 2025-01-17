efa05d9eccb1bfa702123761230a38cdc1f10fc0
Timofeev Yury <yurik-007@mail.ru>
2025-01-17 22:43:25 +0300

    SCRUM-24 feat: delete comments

--- src/components/dialog/Dialog.stories.tsx
+++ src/components/dialog/Dialog.stories.tsx
@@ -1,10 +1,13 @@
-import { Meta, StoryObj } from '@storybook/react'
-import { Dialog, DialogProps } from './Dialog'
 import React, { useState } from 'react'
-import { Button } from '../button/Button'
-import s from './dialog.module.scss'
-import { mockData, mockText } from './mock'
+
 import { Typography } from '@/src/components/typography/Typography'
+import { Meta, StoryObj } from '@storybook/react'
+
+import s from './dialog.module.scss'
+
+import { Button } from '../button/Button'
+import { Dialog, DialogProps } from './Dialog'
+import { mockData, mockText } from './mock'
 
 const meta = {
   argTypes: {},
@@ -38,7 +41,7 @@ const DialogWrapper = (props: DialogProps) => {
           <Button onClick={openModalHandler}>Sign Up</Button>
         </div>
       </div>
-      <Dialog open={showDialog} onClose={closeModalHandler} modalTitle={mockData[0].modalTitle}>
+      <Dialog modalTitle={mockData[0].modalTitle} onClose={closeModalHandler} open={showDialog}>
         <div className={s.boxDiscription}>
           <div className={s.image}></div>
           <Typography option={'regular_text16'}>{mockData[0].modalDescription}</Typography>
@@ -58,126 +61,3 @@ const DialogWrapper = (props: DialogProps) => {
 export const DeleteFollowing: Story = {
   render: args => <DialogWrapper {...args} />,
 }
-
-/*
-export const DialogUsage: Story = {
-  render: () => (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your account and remove your
-            data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  ),
-}
-export const DialogPreview: Story = {
-  render: () => (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="bordered">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className={clsx(s.dialogContent)}>
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className={clsx(s.form)}>
-          <div className={clsx(s.formRow)}>
-            <Typography
-              option={'regular_text14'}
-              as={'label'}
-              className={clsx(s.label)}
-              htmlFor="name"
-            >
-              Name
-            </Typography>
-            <Input id="name" defaultValue="Pedro Duarte" className={clsx(s.input)} />
-          </div>
-          <div className={clsx(s.formRow)}>
-            <Typography
-              option={'regular_text14'}
-              as={'label'}
-              className={clsx(s.label)}
-              htmlFor="username"
-            >
-              Name
-            </Typography>
-            <Input id="username" defaultValue="@peduarte" className={clsx(s.input)} />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  ),
-}
-*/
-
-/*
-export const DialogDemo: Story = {
-  render: () => (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="bordered" className={clsx(s.button, 'violet')}>
-          Edit Profile
-        </Button>
-      </DialogTrigger>
-      <DialogPortal>
-        <DialogOverlay className={clsx(s.overlay)} />
-        <DialogContent className={clsx(s.content)}>
-          <DialogHeader>
-            <DialogTitle className={clsx(s.title)}>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-
-          <div className={clsx(s.fieldset)}>
-            <Typography
-              option={'regular_text14'}
-              as={'label'}
-              className={clsx(s.label)}
-              htmlFor="name"
-            >
-              Name
-            </Typography>
-            <Input id="name" defaultValue="Pedro Duarte" className={clsx(s.fieldset)} />
-          </div>
-          <div className={clsx(s.fieldset)}>
-            <Typography
-              option={'regular_text14'}
-              as={'label'}
-              className={clsx(s.label)}
-              htmlFor="username"
-            >
-              Username
-            </Typography>
-            <Input id="username" defaultValue="@peduarte" className={clsx(s.fieldset)} />
-          </div>
-          <DialogFooter className={clsx(s.footer)}>
-            <DialogClose>
-              <Button type="submit" className={clsx(s.button, 'green')}>
-                Save changes
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-          <DialogClose>
-            <Button type="submit" className={clsx(s.iconButton)} aria-label="Close">
-              <Close />
-            </Button>
-          </DialogClose>
-        </DialogContent>
-      </DialogPortal>
-    </Dialog>
-  ),
-}
-*/
--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,103 +1,37 @@
-/*
-'use client'
-
-import { ComponentPropsWithoutRef, ElementRef, forwardRef, HTMLAttributes } from 'react'
-import { Content, Description, Overlay, Portal, Root, Title, Trigger } from '@radix-ui/react-dialog'
-import Close from '@/src/assets/componentsIcons/CloseOutline'
-import clsx from 'clsx'
-import s from './dialog.module.scss'
-
-const Dialog = Root
-
-const DialogTrigger = Trigger
-
-const DialogPortal = Portal
-
-const DialogClose = Close
-
-const DialogOverlay = forwardRef<
-  ElementRef<typeof Overlay>,
-  ComponentPropsWithoutRef<typeof Overlay>
->(({ className, ...props }, ref) => (
-  <Overlay ref={ref} className={clsx(s.overlay, className)} {...props} />
-))
-DialogOverlay.displayName = Overlay.displayName
-
-const DialogContent = forwardRef<
-  ElementRef<typeof Content>,
-  ComponentPropsWithoutRef<typeof Content>
->(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <Content ref={ref} className={clsx(s.content, className)} {...props}>
-      {children}
-      <Close className={clsx(s.closeButton, className)}>
-        <Close className={clsx(s.icon)} />
-      </Close>
-    </Content>
-  </DialogPortal>
-))
-DialogContent.displayName = Content.displayName
-
-const DialogTitle = forwardRef<ElementRef<typeof Title>, ComponentPropsWithoutRef<typeof Title>>(
-  ({ className, ...props }, ref) => (
-    <Title ref={ref} className={clsx(s.title, className)} {...props} />
-  )
-)
-DialogTitle.displayName = Title.displayName
-
-const DialogDescription = forwardRef<
-  ElementRef<typeof Description>,
-  ComponentPropsWithoutRef<typeof Description>
->(({ className, ...props }, ref) => (
-  <Description ref={ref} className={clsx(s.description, className)} {...props} />
-))
-DialogDescription.displayName = Description.displayName
-
-export {
-  Dialog,
-  DialogPortal,
-  DialogOverlay,
-  DialogClose,
-  DialogTrigger,
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-}
-*/
 import React, { ComponentPropsWithoutRef } from 'react'
-import { Content, Overlay, Portal, Root, Title, Close } from '@radix-ui/react-dialog'
-import s from './dialog.module.scss'
-import CloseOutline from '@/src/assets/componentsIcons/CloseOutline'
 
-import clsx from 'clsx'
+import CloseOutline from '@/src/assets/componentsIcons/CloseOutline'
 import { Typography } from '@/src/components/typography/Typography'
+import { Close, Content, Overlay, Portal, Root, Title } from '@radix-ui/react-dialog'
+import clsx from 'clsx'
+
+import s from './dialog.module.scss'
 
 type DialogSize = 'lg' | 'md' | 'sm'
 
 export type DialogProps = {
-  open: boolean
-  onClose: () => void
   modalTitle: string
+  onClose: () => void
+  open: boolean
   size?: DialogSize
 } & ComponentPropsWithoutRef<'div'>
 
 export const Dialog = ({
+  children,
+  className,
   modalTitle,
   onClose,
-  className,
   open,
   size = 'md',
-  children,
   ...rest
 }: DialogProps) => {
   return (
-    <Root open={open} onOpenChange={onClose} {...rest}>
+    <Root onOpenChange={onClose} open={open} {...rest}>
       <div className={s.Test}></div>
       <Portal>
         <Overlay className={s.overlay} />
         <Content className={clsx(s.content, s[size], className)}>
-          <Title className={s.title} asChild>
+          <Title asChild className={s.title}>
             <Typography as={'h1'} option={'h1'}>
               {modalTitle}
             </Typography>
@@ -105,7 +39,7 @@ export const Dialog = ({
           <hr className={s.lineHr} />
           {children}
           <Close asChild>
-            <button className={s.IconButton} aria-label="Close">
+            <button aria-label={'Close'} className={s.IconButton}>
               <CloseOutline className={clsx(s.icon)} />
             </button>
           </Close>
