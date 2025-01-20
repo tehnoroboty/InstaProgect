b9dc40a527773a5c324335278407a76b7dbd2c36
Anton-Osip <osipchika@gmail.com>
2025-01-20 15:44:57 +0300

    feat: add jsDocs

--- src/components/button/Button.stories.ts
+++ src/components/button/Button.stories.ts
@@ -2,6 +2,31 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button } from './Button'
 
+/**
+ * Button component.
+ *
+ * @param {React.ElementType} [as] - The type of element to be used (e.g., 'button', 'a', etc.).
+ *
+ * @param {boolean} [fullWidth] - Indicates whether the button should take the full width of the container.
+ *
+ * @param {'primary' | 'secondary'} [variant] - The style variant of the button.
+ *
+ * @param {boolean} [loading] - Indicates whether the button is in a loading state.
+ *
+ * @param {boolean} [disabled] - Indicates whether the button is disabled.
+ *
+ * @param {string} [backgroundColor] - The background color of the button.
+ *
+ * @param {string} [color] - The text color of the button.
+ *
+ * @param {React.MouseEventHandler<HTMLButtonElement>} [onClick] - Click event handler.
+ *
+ * @param {React.FocusEventHandler<HTMLButtonElement>} [onFocus] - Focus event handler.
+ *
+ * @param {React.ReactNode} children - The content of the button.
+ *
+ */
+
 const meta = {
   argTypes: {
     variant: {
