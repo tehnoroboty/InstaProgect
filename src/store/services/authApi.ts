4971efe545f11107af4cc51bf7a605bc2ab881cc
Andrei Huryn <gurin.nord@gmail.com>
2025-01-26 21:52:55 +0300

    feat: add registration endpoint to authApi

--- src/store/services/authApi.ts
+++ src/store/services/authApi.ts
@@ -2,7 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-  endpoints: builder => ({}),
+  endpoints: builder => ({
+    registration: builder.mutation<any, any>({
+      query: payload => ({
+        body: payload,
+        method: 'POST',
+        url: 'auth/registration',
+      }),
+    }),
+  }),
   reducerPath: 'authApi',
 })
 
