7b83cdf8f8c252415ecb92a395eae677c1405f8d
Anton-Osip <osipchika@gmail.com>
2025-01-27 19:13:14 +0300

    feat: data acquisition

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import { Button } from '@/src/components/button/Button'
 import { Card } from '@/src/components/card/Card'
@@ -7,7 +10,18 @@ import { Typography } from '@/src/components/typography/Typography'
 
 import s from './page.module.scss'
 
+type FormValues = {
+  email: string
+  password: string
+}
+
 export default function LoginPage() {
+  const { handleSubmit, register } = useForm<FormValues>()
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
+
   return (
     <div className={s.wrapper}>
       <Card className={s.card}>
@@ -18,28 +32,44 @@ export default function LoginPage() {
           <div>Google</div>
           <div>Github</div>
         </div>
-        <div className={s.boxInputs}>
-          <Input className={s.input} label={'Email'} placeholder={'Epam@epam.com'} />
+        <form onSubmit={onSubmit}>
+          <Input className={s.input} label={'Email'} {...register('email')} />
           <Input
             className={s.input}
             label={'Password'}
             placeholder={'**********'}
             type={'password'}
+            {...register('password')}
           />
-        </div>
-        <div className={s.boxLinks}>
           <Button as={'a'} className={s.forgotPassword} href={'#'} variant={'transparent'}>
             Forgot Password
           </Button>
-          <Button className={s.singIn} fullWidth variant={'primary'}>
+          <Button className={s.singIn} fullWidth type={'submit'} variant={'primary'}>
             Sing in
           </Button>
-          <Typography className={s.text} option={'regular_text16'}>
-            Don’t have an account?
-          </Typography>
-          <Button className={s.singUp} variant={'transparent'}>
-            {'Sing up'}
-          </Button>
+        </form>
+        {/*<div className={s.boxInputs}>*/}
+        {/*  <Input className={s.input} label={'Email'} placeholder={'Epam@epam.com'} />*/}
+        {/*  <Input*/}
+        {/*    className={s.input}*/}
+        {/*    label={'Password'}*/}
+        {/*    placeholder={'**********'}*/}
+        {/*    type={'password'}*/}
+        {/*  />*/}
+        {/*</div>*/}
+        <div className={s.boxLinks}>
+          {/*<Button as={'a'} className={s.forgotPassword} href={'#'} variant={'transparent'}>*/}
+          {/*  Forgot Password*/}
+          {/*</Button>*/}
+          {/*<Button className={s.singIn} fullWidth variant={'primary'}>*/}
+          {/*  Sing in*/}
+          {/*</Button>*/}
+          {/*<Typography className={s.text} option={'regular_text16'}>*/}
+          {/*  Don’t have an account?*/}
+          {/*</Typography>*/}
+          {/*<Button className={s.singUp} variant={'transparent'}>*/}
+          {/*  {'Sing up'}*/}
+          {/*</Button>*/}
         </div>
       </Card>
     </div>
