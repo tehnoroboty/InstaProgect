81b47a9047bf92bee55fe52c7a7be459a2d8dc05
Timofeev Yury <yurik-007@mail.ru>
2025-02-02 21:29:25 +0300

    SCRUM-28 feat: add handleError

--- src/store/services/baseApi.ts
+++ src/store/services/baseApi.ts
@@ -10,31 +10,6 @@ export const baseApi = createApi({
         headers.set('Authorization', `Bearer ${localStorage.getItem('sn-token')}`)
       },
     })(args, api, extraOptions)
-    let error = 'Some error occurred'
-
-    if (result.error) {
-      switch (result.error.status) {
-        case 'FETCH_ERROR':
-        case 'PARSING_ERROR':
-        case 'CUSTOM_ERROR':
-          error = result.error.error
-          break
-
-        case 403:
-          error = '403 Forbidden Error. Check API-KEY'
-          break
-
-        case 400:
-        case 500:
-          error = (result.error.data as { message: string }).message
-          break
-
-        default:
-          error = JSON.stringify(result.error)
-          break
-      }
-      api.dispatch(setAppError({ error }))
-    }
 
     return result
   },
---
+++ src/utils/handleError.ts
@@ -0,0 +1,41 @@
+import { setAppError } from '@/src/store/Slices/appSlice'
+import {
+  BaseQueryApi,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+  QueryReturnValue,
+} from '@reduxjs/toolkit/query'
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
+) => {
+  let error = 'Some error occurred'
+
+  if (result.error) {
+    switch (result.error.status) {
+      case 'FETCH_ERROR':
+      case 'PARSING_ERROR':
+      case 'CUSTOM_ERROR':
+        error = result.error.error
+        break
+
+      case 401:
+        error = '401 Unauthorized'
+        break
+
+      case 429:
+        error = 'More than 5 attempts from one IP-address during 10 seconds'
+        break
+
+      case 500:
+        error = (result.error.data as { message: string }).message
+        break
+
+      default:
+        error = JSON.stringify(result.error)
+        break
+    }
+    api.dispatch(setAppError({ error }))
+  }
+}
