0ae896a2995b21beb432b0297bd22008c56bbebc
Арсений <ars4112@gmail.com>
2025-02-09 19:07:15 +0300

    SCRUM-59 refactor: transfering GooglePage to the Features folder

---
+++ src/features/oauth/google/GooglePage.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { setAppError } from '@/src/store/Slices/appSlice'
+import { useExchangeGoogleCodeForTokenMutation } from '@/src/store/services/authApi'
+import { useRouter, useSearchParams } from 'next/navigation'
+import PropagateLoader from 'react-spinners/PropagateLoader'
+
+export const GooglePage = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const [exchangeGoogleCodeForToken, { data, error }] = useExchangeGoogleCodeForTokenMutation()
+
+  useEffect(() => {
+    const code = searchParams.get('code')
+
+    if (!code) {
+      router.push('/auth/login')
+    } else {
+      exchangeGoogleCodeForToken({ code, redirectUrl: 'https://momenttify.store/auth/google' })
+        .unwrap()
+        .catch(err => {
+          dispatch(setAppError({ error: err.data.messages[0].message }))
+        })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (error) {
+      router.push('/auth/registration')
+    } else {
+      if (data) {
+        localStorage.setItem('sn-token', data.accessToken)
+        router.push('/home')
+      }
+    }
+  }, [error, data])
+
+  return (
+    <PropagateLoader
+      color={'#ffffff'}
+      cssOverride={{
+        alignItems: 'center',
+        color: '#ffffff',
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'center',
+      }}
+      size={20}
+    />
+  )
+}
