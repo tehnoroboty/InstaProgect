b6012a22c196a0d02d7cc96baccd415532db4fc6
dnemirich <dnemirich@gmail.com>
2025-02-17 22:21:40 +0100

    SCRUM-51 fix: fix store middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { appReducer, appSlice } from '@/src/store/Slices/appSlice'
 import { authApi } from '@/src/store/services/authApi'
+import { baseApi } from '@/src/store/services/baseApi'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const initializeStore = () => {
   return configureStore({
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(authApi.middleware),
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
     reducer: {
       [appSlice.name]: appReducer,
       [authApi.reducerPath]: authApi.reducer,
