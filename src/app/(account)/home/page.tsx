4ecf729816e4b1e4b44970e8e8d5b28f4c0933d8
KlimovaED <klimovaed@icloud.com>
2025-02-21 18:41:26 +0400

    SCRUM-54 fix: modal add photo

--- src/app/(account)/home/page.tsx
+++ src/app/(account)/home/page.tsx
@@ -2,7 +2,7 @@ import { CreatePost } from '@/src/components/createPost/CreatePost'
 
 export default function HomePage() {
   return (
-    <div style={{ color: 'red', display: 'flex' }}>
+    <div>
       <CreatePost />
     </div>
   )
--- src/components/createPost/CreatePost.tsx
+++ src/components/createPost/CreatePost.tsx
@@ -1,6 +1,10 @@
 'use client'
+import { useState } from 'react'
+import { useDropzone } from 'react-dropzone'
 
+import ImageOutline from '@/src/assets/componentsIcons/ImageOutline'
 import { Content } from '@radix-ui/react-dialog'
+import Image from 'next/image'
 
 import s from './createPost.module.scss'
 
@@ -8,15 +12,42 @@ import { Button } from '../button/Button'
 import { Dialog } from '../dialog/Dialog'
 
 export const CreatePost = () => {
+  const [photo, setPhoto] = useState<null | string>(null)
+
+  const { getInputProps, getRootProps, open } = useDropzone({
+    accept: {
+      'image/jpeg': ['.jpg', '.jpeg'],
+      'image/png': ['.png'],
+    },
+    multiple: false,
+    noClick: true,
+    onDrop: acceptedFiles => {
+      const file = acceptedFiles[0]
+      const fileUrl = URL.createObjectURL(file)
+
+      setPhoto(fileUrl)
+    },
+  })
+
   return (
     <div>
       <Dialog className={s.modal} modalTitle={'Add Photo'} onClose={() => {}} open>
-        <Content>
+        <div className={s.content}>
+          {photo ? (
+            <Image alt={'img'} height={300} src={photo} width={300} />
+          ) : (
+            <div className={s.imageBox}>
+              <ImageOutline height={48} width={48} />
+            </div>
+          )}
           <div className={s.buttons}>
-            <Button variant={'primary'}>{'Select from Computer'}</Button>
+            <Button onClick={open} variant={'primary'}>
+              <input {...getInputProps()} />
+              {'Select from Computer'}
+            </Button>
             <Button variant={'bordered'}>{'Open Draft'}</Button>
           </div>
-        </Content>
+        </div>
       </Dialog>
     </div>
   )
