6fcb1f2062037d054b0e234ebda1243071f6bbff
dnemirich <dnemirich@gmail.com>
2025-02-23 16:15:52 +0100

    SCRUM-51 fix: fix me conflicts

--- src/store/services/baseApi.ts
+++ src/store/services/baseApi.ts
@@ -119,4 +119,5 @@ export const baseApi = createApi({
   baseQuery: baseQueryWithReauth, // Используем кастомный baseQuery
   endpoints: () => ({}),
   reducerPath: 'inctagramApi',
+  tagTypes: ['ME'],
 })
