2e2a85d5d67870b7f48e627ff12d9448d03483fe
Timofeev Yury <yurik-007@mail.ru>
2025-03-06 10:32:39 +0300

    SCRUM-88 feat: check lints

--- src/shared/ui/Scrollbar/Scrollbar.tsx
+++ src/shared/ui/Scrollbar/Scrollbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
 import {
   Corner,
   Root,
@@ -9,17 +10,19 @@ import {
   Viewport,
 } from '@radix-ui/react-scroll-area'
 import clsx from 'clsx'
+
 import s from './scrollbar.module.scss'
 
 const ScrollArea = forwardRef<ElementRef<typeof Root>, ComponentPropsWithoutRef<typeof Root>>(
-  ({ className, children, ...rest }, ref) => (
-    <Root ref={ref} className={clsx(s.scrollArea, className)} {...rest}>
+  ({ children, className, ...rest }, ref) => (
+    <Root className={clsx(s.scrollArea, className)} ref={ref} {...rest}>
       <Viewport className={s.viewport}>{children}</Viewport>
       <ScrollBar />
       <Corner />
     </Root>
   )
 )
+
 ScrollArea.displayName = Root.displayName
 
 const ScrollBar = forwardRef<
@@ -27,9 +30,9 @@ const ScrollBar = forwardRef<
   ComponentPropsWithoutRef<typeof ScrollAreaScrollbar>
 >(({ className, orientation = 'vertical', ...rest }, ref) => (
   <ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
     className={clsx(s.scrollBar, orientation === 'vertical' ? s.vertical : s.horizontal, className)}
+    orientation={orientation}
+    ref={ref}
     {...rest}
   >
     <ScrollAreaThumb
@@ -39,6 +42,7 @@ const ScrollBar = forwardRef<
     />
   </ScrollAreaScrollbar>
 ))
+
 ScrollBar.displayName = ScrollAreaScrollbar.displayName
 
 export { ScrollArea, ScrollBar }
