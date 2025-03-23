853f4910b83ed2d00982dd3f704684d97302c302
Anton-Osip <osipchika@gmail.com>
2025-03-23 16:03:23 +0300

    SCRUM-57: feat: get user posts in public Profile page

--- src/app/public/profile/[userId]/page.tsx
+++ src/app/public/profile/[userId]/page.tsx
@@ -1,4 +1,7 @@
+import React from "react";
+
 import {PublicProfileTypes} from "@/src/entities/user/types";
+import {GetMyPostsResponse} from "@/src/shared/model/api/types";
 import {PublicProfile} from "@/src/widgets/profile/PublicProfile/PublicProfile";
 
 type Params = {
@@ -18,10 +21,9 @@ const getUserProfile = async (userId: string): Promise<PublicProfileTypes> => {
     return await res.json()
 }
 
-const getUserPosts = async (userId: string): Promise<PublicProfileTypes> => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}public-user/profile/${userId}`, {
+const getUserPosts = async (userId: string): Promise<GetMyPostsResponse> => {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}public-posts/user/${userId}`, {
         cache: 'no-store',
-
     })
 
     return await res.json()
@@ -31,9 +33,12 @@ const getUserPosts = async (userId: string): Promise<PublicProfileTypes> => {
 export default async function PublicProfilePage({params}: Props) {
 
     const userProfile = await getUserProfile(params.userId)
+    const userPosts = await getUserPosts(params.userId)
 
 
-    return <PublicProfile userProfile = {userProfile}/>
+    return <>
+        <PublicProfile userPosts = {userPosts} userProfile = {userProfile}/>
+    </>
 
 
 }
--- src/widgets/profile/PublicProfile/PublicProfile.tsx
+++ src/widgets/profile/PublicProfile/PublicProfile.tsx
@@ -1,16 +1,24 @@
+import React from 'react'
+
 import { PublicProfileTypes } from '@/src/entities/user/types'
+import { GetMyPostsResponse } from '@/src/shared/model/api/types'
+import { Posts } from '@/src/shared/ui/postsGrid/Posts'
 import { ProfileInfo } from '@/src/widgets/profile/PublicProfile/profileInfo/ProfileInfo'
 
 import s from '../myProfile.module.scss'
 
 type Props = {
+  userPosts: GetMyPostsResponse
   userProfile: PublicProfileTypes
 }
 
-export const PublicProfile = ({ userProfile }: Props) => {
+export const PublicProfile = ({ userPosts, userProfile }: Props) => {
+  const { items: posts } = userPosts
+
   return (
     <div className={s.page}>
       <ProfileInfo publicUserProfile={userProfile} />
+      {posts.length && <Posts posts={posts} />}
     </div>
   )
 }
