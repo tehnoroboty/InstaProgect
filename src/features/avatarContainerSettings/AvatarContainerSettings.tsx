f151d7ee3009b702f83e5feedd2ec138cceb672e
Timofeev Yury <yurik-007@mail.ru>
2025-04-07 02:02:45 +0300

    SCRUM-Settings_sub feat: add modal

--- src/features/avatarContainerSettings/AvatarContainerSettings.tsx
+++ src/features/avatarContainerSettings/AvatarContainerSettings.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 import { Avatar } from '@/src/entities/user/types'
+import { CreatePost } from '@/src/features/createPost/CreatePost'
 import { AvatarBox } from '@/src/shared/ui/avatar/AvatarBox'
 import { Button } from '@/src/shared/ui/button/Button'
 import clsx from 'clsx'
@@ -11,6 +13,7 @@ type Props = {
   deleteModal: () => void
   isLoadingDelete: boolean
   myProfileAvatars: Array<Avatar> | undefined
+  uploadAvatar: (file: File) => void
 }
 
 export const AvatarContainerSettings = ({
@@ -18,6 +21,20 @@ export const AvatarContainerSettings = ({
   isLoadingDelete,
   myProfileAvatars,
 }: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false) // Состояние для управления открытием модалки
+  const [newAvatar, setNewAvatar] = useState<null | string>(null) // Состояние для хранения загруженного фото
+
+  // Открытие модалки для выбора фото
+  const handleAddPhoto = () => {
+    setIsModalOpen(true)
+  }
+
+  // Функция для обновления аватара (когда пользователь выбрал фото)
+  const handlePhotoSelect = (photoUrl: string) => {
+    setNewAvatar(photoUrl) // Устанавливаем загруженное фото
+    setIsModalOpen(false) // Закрываем модалку
+  }
+
   return (
     <div className={s.photoBox}>
       <div className={s.avatarContainer}>
@@ -31,9 +48,10 @@ export const AvatarContainerSettings = ({
           ></button>
         )}
       </div>
-      <Button type={'button'} variant={'bordered'}>
+      <Button onClick={handleAddPhoto} type={'button'} variant={'bordered'}>
         {'Add a Profile Photo'}
       </Button>
+      {isModalOpen && <CreatePost download={handlePhotoSelect} />}
     </div>
   )
 }
--- src/widgets/generationInformation/GenerationInformation.tsx
+++ src/widgets/generationInformation/GenerationInformation.tsx
@@ -267,6 +267,13 @@ export const GenerationInformation = () => {
             deleteModal={() => setDeleteModal(true)}
             isLoadingDelete={isLoadingDelete}
             myProfileAvatars={MyProfile?.avatars}
+            uploadAvatar={file => {
+              // Здесь загрузка файла через RTK Query или fetch/post
+              const formData = new FormData()
+
+              formData.append('file', file)
+              // dispatch(uploadAvatar(formData))
+            }}
           />
           <div className={s.informationBox}>
             <Input
