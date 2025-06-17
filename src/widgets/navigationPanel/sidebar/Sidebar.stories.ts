d35d9cda20d24b19e59aaa74f6fed85b867d823c
Anton-Osip <osipchika@gmail.com>
2025-06-17 19:27:16 +0300

    dev prod:fix error

--- src/widgets/navigationPanel/sidebar/Sidebar.stories.ts
+++ src/widgets/navigationPanel/sidebar/Sidebar.stories.ts
@@ -1,7 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { MenuItemsType, menuItems } from '@/src/widgets/navigationPanel/NavigationPanel'
 import Sidebar from '@/src/widgets/navigationPanel/sidebar/Sidebar'
+import {MenuItemsType} from "@/src/widgets/navigationPanel/types";
+import {
+  Bookmark,
+  BookmarkOutline,
+  Home,
+  HomeLine, LogOutOutline, MessageCircle, MessageCircleOutline, Person, PersonOutline, PlusSquare,
+  PlusSquareOutline, SearchOutline,
+  TrendingUpOutline
+} from "@/src/shared/assets/componentsIcons";
 
 const meta = {
   component: Sidebar,
@@ -12,14 +20,47 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const menuItemsMock: MenuItemsType = menuItems
+const menuItemsMock:  MenuItemsType = {
+  additional: [
+    { href: '/statistics', icon: TrendingUpOutline, title: 'Statistics' },
+    {
+      href: '/favorites',
+      icon: BookmarkOutline,
+      iconActive: Bookmark,
+      title: 'Favorites',
+    },
+  ],
+  mainActions: [
+    { href: '/', icon: HomeLine, iconActive: Home, title: 'Home' },
+    {
+      icon: PlusSquareOutline,
+      iconActive: PlusSquare,
+      onClick: ()=>{},
+      title: 'Create',
+    },
+    {
+      href: `/profile/id`,
+      icon: PersonOutline,
+      iconActive: Person,
+      title: 'My Profile',
+    },
+    {
+      href: '/messenger',
+      icon: MessageCircleOutline,
+      iconActive: MessageCircle,
+      title: 'Messenger',
+    },
+    { href: '/search', icon: SearchOutline, title: 'Search' },
+  ],
+  usersActions: [{ icon: LogOutOutline, title: 'Log Out' }],
+}
 
 export const Primary: Story = {
   args: {
     items: {
       additional: menuItemsMock.additional,
       mainActions: menuItemsMock.mainActions,
-      usersActions: menuItems.usersActions,
+      usersActions: menuItemsMock.usersActions,
     },
   },
 }
