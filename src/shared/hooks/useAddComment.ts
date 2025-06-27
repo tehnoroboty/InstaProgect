c3214a749e77b3536f3da2f52f7ed6dfbb0a04a1
Andrei Huryn <gurin.nord@gmail.com>
2025-06-27 12:03:14 +0300

    feat: add useAddComment hook

---
+++ src/shared/hooks/useAddComment.ts
@@ -0,0 +1,35 @@
+import { ChangeEvent, useState } from 'react'
+
+import { useCreateNewCommentMutation } from '@/src/shared/model/api/commentsAnswersApi'
+
+export const useAddComment = (postId: number) => {
+  const [commentText, setCommentText] = useState('')
+
+  const [createNewComment, { isError, isLoading }] = useCreateNewCommentMutation()
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCommentText(e.target.value)
+  }
+
+  const handleSubmit = async () => {
+    if (!commentText.trim()) {
+      return
+    }
+
+    try {
+      const newComment = await createNewComment({ content: commentText, postId }).unwrap()
+
+      setCommentText('')
+    } catch (err) {
+      console.error('Error adding comment:', err)
+    }
+  }
+
+  return {
+    commentText,
+    handleChange,
+    handleSubmit,
+    isError,
+    isLoading,
+  }
+}
