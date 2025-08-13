ee7687ef156aabd306ab42be8317a0e99508026d
Timofeev Yury <yurik-007@mail.ru>
2025-08-13 23:23:09 +0300

    SCRUM-178 fix: remove console.log error warn and comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { GetCommentsResponse } from '@/src/entities/comments/types'
 import { Post, PublicPostsResponse } from '@/src/entities/post/types'
 import { UsersCountResponse } from '@/src/entities/users/types'
-import { setAppError } from '@/src/shared/model/slices/appSlice'
-import { useAppDispatch } from '@/src/shared/model/store/store'
 import { SearchParams, getUserComments, getUserPost } from '@/src/widgets/profile/getPublicProfile'
 import { PublicFeed } from '@/src/widgets/publicFeed/PublicFeed'
 import { getUsersCount, getUsersPosts } from '@/src/widgets/publicFeed/getPublicPosts'
@@ -11,7 +9,6 @@ export default async function Page(props: {
   params: { postId: string }
   searchParams: SearchParams
 }) {
-  const dispatch = useAppDispatch()
   const publicPosts: PublicPostsResponse = await getUsersPosts()
   const usersCount: UsersCountResponse = await getUsersCount()
   const searchParams = props.searchParams
@@ -26,9 +23,7 @@ export default async function Page(props: {
         getUserComments(Number(query)),
       ])
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : String(error)
-
-      dispatch(setAppError({ error: errorMessage }))
+      console.error('Failed to fetch post:', error)
     }
   }
 
