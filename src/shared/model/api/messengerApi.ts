59ce0f48729eb91d5dbb174e0646e780a5c02396
Andrei Huryn <gurin.nord@gmail.com>
2025-08-23 12:56:10 +0300

    feat: add handling error to getMessagesByUser in messengerApi

--- src/shared/model/api/messengerApi.ts
+++ src/shared/model/api/messengerApi.ts
@@ -1,3 +1,4 @@
+import { CustomerError } from '@/src/entities/errors/types'
 import {
   GetAllMessagesArgs,
   GetAllMessagesResponse,
@@ -5,6 +6,7 @@ import {
   GetMessagesByUserResponse,
 } from '@/src/entities/messenger/types'
 import { baseApi } from '@/src/shared/model/api/baseApi'
+import { setAppError } from '@/src/shared/model/slices/appSlice'
 
 export const messengerApi = baseApi.injectEndpoints({
   endpoints: builder => ({
@@ -21,6 +23,18 @@ export const messengerApi = baseApi.injectEndpoints({
       }),
     }),
     getMessagesByUser: builder.query<GetMessagesByUserResponse, GetMessagesByUserArgs>({
+      async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled
+          dispatch(setAppError({ error: null }))
+        } catch (err) {
+          const error = err as CustomerError
+          const errorMessage =
+            error.data?.messages?.[0]?.message ?? error.data?.error ?? 'Unknown error'
+
+          dispatch(setAppError({ error: errorMessage }))
+        }
+      },
       providesTags: (_result, _error, arg) => [{ id: arg.dialoguePartnerId, type: 'MESSAGES' }],
       query: ({ cursor, dialoguePartnerId, pageSize = 12, searchName }) => ({
         method: 'GET',
