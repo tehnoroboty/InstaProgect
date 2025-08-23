1402159eddf9847a72cea31d2e00745ada4d36af
Timofeev Yury <yurik-007@mail.ru>
2025-08-23 10:54:16 +0300

    Hot fix remove RequireAuth

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -2,9 +2,5 @@ import { RequireAuth } from '@/src/features/requireAuth/RequireAuth'
 import { ProtectedFeed } from '@/src/widgets/protectedFeed/ProtectedFeed'
 
 export default function Feed() {
-  return (
-    <RequireAuth>
-      <ProtectedFeed />
-    </RequireAuth>
-  )
+  return <ProtectedFeed />
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,9 +2,5 @@ import { RequireAuth } from '@/src/features/requireAuth/RequireAuth'
 import { ShowSearch } from '@/src/features/showSearch/ShowSearch'
 
 export default function SearchPage() {
-  return (
-    <RequireAuth>
-      <ShowSearch />
-    </RequireAuth>
-  )
+  return <ShowSearch />
 }
