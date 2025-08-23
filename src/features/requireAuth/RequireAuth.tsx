f9c55f1710f33b156506b139762ab64dbb4d2805
Anton-Osip <osipchika@gmail.com>
2025-08-23 19:46:30 +0300

    hot-fix: fix RequireAuth

--- src/features/requireAuth/RequireAuth.tsx
+++ src/features/requireAuth/RequireAuth.tsx
@@ -3,13 +3,7 @@
 import { ReactNode, useEffect } from 'react'
 
 import { AuthRoutes } from '@/src/shared/lib/constants/routing'
-import { useMeQuery } from '@/src/shared/model/api/authApi'
-import { selectIsLoggedIn } from '@/src/shared/model/slices/appSlice'
-import { useAppSelector } from '@/src/shared/model/store/store'
-import { Loader } from '@/src/shared/ui/loader/Loader'
 import { useRouter } from 'next/navigation'
-
-import s from './requireAuth.module.scss'
 
 type Props = {
   children: ReactNode
@@ -17,32 +11,14 @@ type Props = {
 
 export const RequireAuth = ({ children }: Props) => {
   const router = useRouter()
-  const isLoggedIn = useAppSelector(selectIsLoggedIn)
-  const { data, isError, isLoading, isSuccess } = useMeQuery(undefined, { skip: !isLoggedIn })
 
   useEffect(() => {
-    if (isError) {
+    const token = localStorage.getItem('accessToken')
+
+    if (!token) {
       router.replace(AuthRoutes.LOGIN)
     }
-  }, [isError, router])
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      router.replace(AuthRoutes.LOGIN)
-    }
-  }, [isLoggedIn, router])
-
-  if (isLoading) {
-    return (
-      <div className={s.wrapper}>
-        <Loader />
-      </div>
-    )
-  }
-
-  if (!data || !isSuccess) {
-    return null
-  }
+  }, [router])
 
   return <>{children}</>
 }
