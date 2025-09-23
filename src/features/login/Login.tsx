0bc7843c7bd4dbc222826651c8002f331c221f6d
Stanislav <kepamuk@yandex.ru>
2025-09-23 17:43:05 +0300

    SCRUM-195: fix: disabled submit button in login page

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import { useEffect } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { LoginError } from '@/src/entities/errors/types'
@@ -26,19 +26,36 @@ export default function Login() {
 
   useSuccessAlert(isSuccess, 'You have successfully logged in.')
   const {
+    clearErrors,
     formState: { errors, isValid },
     handleSubmit,
     register,
     setError,
+    watch,
   } = useForm<FormType>({
     defaultValues: {
       email: 'tehnoroboty@gmail.com',
       password: 'qwQW12!',
     },
-    mode: 'onBlur',
+    mode: 'onChange',
     resolver: zodResolver(schema),
   })
   const disabledButton = isLoading || !isValid || Object.keys(errors).length > 0
+
+  const emailValue = watch('email')
+  const passwordValue = watch('password')
+
+  useEffect(() => {
+    if (emailValue && errors.email) {
+      clearErrors('email')
+    }
+  }, [emailValue, errors.email, clearErrors])
+
+  useEffect(() => {
+    if (passwordValue && errors.password) {
+      clearErrors('password')
+    }
+  }, [passwordValue, errors.password, clearErrors])
 
   const onSubmit: SubmitHandler<FormType> = async formData => {
     try {
