69492db9734dad04076fb0eb0b0aff753b4fcd46
Andrei Huryn <gurin.nord@gmail.com>
2025-09-24 18:26:04 +0300

    feat: add validation to email length

--- src/shared/lib/constants/error-messages.ts
+++ src/shared/lib/constants/error-messages.ts
@@ -5,6 +5,8 @@ export const ERROR_MESSAGES = {
   EMAIL: {
     FORMAT: 'The email must match the format example@example.com',
     INVALID: 'Invalid email address',
+    MAX: 'The email must contain a maximum of 50 characters',
+    MIN: 'The email must contain at least 6 characters',
     REQUIRED: 'Email is required',
   },
   FIRST_NAME: {
